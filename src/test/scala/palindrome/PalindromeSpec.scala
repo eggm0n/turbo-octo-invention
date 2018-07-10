aa33029908e1b0bcacd49635960f41d84af32acb
James Farlow <james@fantoo.net>
2018-07-10 21:10:51 +0100

    refactored the spec for better readability

--- src/test/scala/palindrome/PalindromeSpec.scala
+++ src/test/scala/palindrome/PalindromeSpec.scala
@@ -3,21 +3,19 @@ package palindrome
 import org.scalatest.{FlatSpec, Matchers}
 
 class PalindromeSpec extends FlatSpec with Matchers with Palindrome {
-  "The Palindrome trait" should "when given the example input produce a list of unique palindromes" in {
-    getTopPalindromes("sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop", 3) should be(
-      List("hijkllkjih", "defggfed", "abccba"))
+  "The Palindrome trait" should "produce a list of unique palindromes when given the example input " in {
+    getTopPalindromes("sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop", 3) should be(List("hijkllkjih", "defggfed", "abccba"))
   }
 
-  it should "when given an input with only one palindrome produce a singleton list of unique palindromes" in {
+  it should "produce a singleton list of unique palindromes when given an input with only one palindrome " in {
     getPalindromes("123454321") should be(List("123454321"))
   }
 
-  it should "when given an input with at least one non-unique palindrome produce a list of unique palindromes" in {
+  it should "produce a list of unique palindromes when given an input with at least one non-unique palindrome" in {
     getPalindromes("123454321x2345432") should be(List("123454321", "x"))
   }
 
-  it should "when given an input with multiple non-unique palindromes separated by unique strings produce a list of the unique palindromes" in {
-    getPalindromes("123454321x2345432y34543") should be(
-      List("123454321", "y", "x"))
+  it should "produce a list of the unique palindromes when given an input with multiple non-unique palindromes separated by unique strings" in {
+    getPalindromes("123454321x2345432y34543") should be(List("123454321", "y", "x"))
   }
 }
